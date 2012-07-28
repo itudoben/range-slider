2d71528520bdf3835343a9a49a78e9978a3c926b
itudoben <itudoben@gmail.com>
2012-07-28 09:07:41 -0400

    MOD: copyright date

--- build.gradle
+++ build.gradle
@@ -1,6 +1,6 @@
 /*
  * RangeSlider allows a range selection from X axis.
- * Copyright (C) <2011-2012>  <itudoben at gmail dot com>
+ * Copyright (C) <2012>  <itudoben at gmail dot com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
